ae9d695dbfdfd7a8513ec822e13bde63c61377ef
Rute Figueiredo <rute@rute-VirtualBox>
2020-06-16 11:28:21 +0100

    added new version of project

--- ecoportugal-app/src/components/CardApp.js
+++ ecoportugal-app/src/components/CardApp.js
@@ -3,12 +3,14 @@ import {Card} from "react-bootstrap";
 
 const CardApp = (props) => {
     return (
-        <Card className="shadow rounded p-3 mt-3">
-            <Card.Img variant="top" src={`https://robohash.org/${props.id}?200x200`} />
-            <Card.Body>
-                <Card.Title className="text-center">{props.name}</Card.Title>
-            </Card.Body>
-        </Card>
+        <a href="">
+            <Card className="shadow rounded p-3 mt-3 grow">
+                <Card.Img variant="top" src={`https://robohash.org/${props.id}?200x200`} />
+                <Card.Body>
+                    <Card.Title className="text-center">{props.name}</Card.Title>
+                </Card.Body>
+            </Card>
+        </a>
     );
 }
 
