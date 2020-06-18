dde7468c0c23e4abab12d5abfccebe8fe26e025e
Rute Figueiredo <rutefig@hotmail.com>
2020-06-18 16:22:41 +0100

    added some improvements for home page

--- ecoportugal-app-v1/src/components/CategoryCard.js
+++ ecoportugal-app-v1/src/components/CategoryCard.js
@@ -4,7 +4,7 @@ import {Card} from "react-bootstrap";
 const CategoryCard = (props) => {
     return (
         <a href={props.name}>
-            <Card className="shadow rounded p-3 mt-3 grow">
+            <Card className="shadow rounded p-3 mt-3 grow bg-dark">
                 <Card.Img variant="top" src={`https://robohash.org/${props.id}?200x200`} />
                 <Card.Body>
                     <Card.Title className="text-center">{props.name}</Card.Title>
