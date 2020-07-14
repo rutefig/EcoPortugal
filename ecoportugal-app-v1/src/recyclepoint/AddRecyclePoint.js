eff7328683b4ad5961d57b9604160d0ed0264334
Rute Figueiredo <rute.figueiredo92@gmail.com>
2020-07-14 17:24:16 +0100

    added location api to add recycle point with location and initial development of add recycle point form

--- ecoportugal-app-v1/src/recyclepoint/AddRecyclePoint.js
+++ ecoportugal-app-v1/src/recyclepoint/AddRecyclePoint.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 
 const AddRecyclePoint = () => {
   return (
     <>
-      <h1>Add Recycle Point With Street Address</h1>
+      <h1 className="title">Adicionar Ponto de Recolha com Morada</h1>
+      <p className="form-information">Por favor tenha atenção que estes dados serão confirmados,
+      por favor coloque tudo o mais certo possível de forma a
+      facilitar a nossa confirmação. Todos os campos são obrigatórios</p>
+
+    <Form>
+      <Form.Group controlId="formName">
+        <Form.Control type="text" placeholder="Nome" />
+        <Form.Text className="text-muted">
+          * Required
+        </Form.Text>
+      </Form.Group>
+      <Form.Group>
+        <Form.Control type="text" placeholder="Localidade" />
+        <Form.Text className="text-muted">
+          * Required
+        </Form.Text>
+      </Form.Group>
+    </Form>
     </>
   );
 }
--- ecoportugal-app-v1/src/recyclepoint/AddRecyclePointWithMap.js
+++ ecoportugal-app-v1/src/recyclepoint/AddRecyclePointWithMap.js
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 
 const AddRecyclePointWithMap = () => {
 
+  const [location, setLocation] = useState({});
+
+  // Conteudo retirado do seguinte link: https://www.youtube.com/watch?v=6WB16wZS61c
+  useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(handlePositionReceived);
+
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, []);
+
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
+
+    setLocation({ latitude, longitude });
+  }
+
   return (
     <>
       <h1>Add Recycle Point With Location</h1>
+      <h3>Latitude: {location.latitude}</h3>
+      <h3>longitude: {location.longitude}</h3>
     </>
   );
 }
