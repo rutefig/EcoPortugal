9f4b54cd191bae33efc09a1978fa9a556ebe0a82
Rute Figueiredo <rute.figueiredo92@gmail.com>
2020-07-18 22:52:46 +0100

    Updates

---
+++ precache-manifest.775d197be21ca27e00a0d4f0f56f2453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb09acfd6a8b09f9719f1b999cde754",
+    "url": "/EcoPortugal/index.html"
+  },
+  {
+    "revision": "ce5fb8ab98f5a392b1ad",
+    "url": "/EcoPortugal/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "76b69c98365831c2b629",
+    "url": "/EcoPortugal/static/css/main.a9186332.chunk.css"
+  },
+  {
+    "revision": "ce5fb8ab98f5a392b1ad",
+    "url": "/EcoPortugal/static/js/2.7e6fb9eb.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/EcoPortugal/static/js/2.7e6fb9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b69c98365831c2b629",
+    "url": "/EcoPortugal/static/js/main.a9be3a71.chunk.js"
+  },
+  {
+    "revision": "4734504f73b1f5bd6284",
+    "url": "/EcoPortugal/static/js/runtime-main.47639c8c.js"
+  }
+]);
