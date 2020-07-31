a38613d7f1dbbb6278f79403986b152c5aaa3a4b
Rute Figueiredo <rute.figueiredo92@gmail.com>
2020-07-31 19:17:55 +0100

    user location on recycle point details page removed

--- ecoportugal-app-v1/src/recyclepoint/RecyclePointPage.js
+++ ecoportugal-app-v1/src/recyclepoint/RecyclePointPage.js
@@ -4,39 +4,21 @@ import MapContainer from "./MapContainer";
 import { Container, Row, Col } from "react-bootstrap";
 
 const RecyclePointPage = () => {
-  const [location, setLocation] = useState({});
-  const [mapProps, setMapProps] = useState({});
-
-  // Conteudo retirado do seguinte link: https://www.youtube.com/watch?v=6WB16wZS61c
-  useEffect(() => {
-    const watchId = navigator.geolocation.watchPosition(handlePositionReceived);
-
-    return () => navigator.geolocation.clearWatch(watchId);
-  }, []);
-
-  function handlePositionReceived({ coords }) {
-    const { latitude, longitude } = coords;
-
-    setLocation({ latitude, longitude });
-  }
 
   let { selectedCategory, selectedPoint, coordinates } = useParams();
   let coords = coordinates.split(",");
 
   // https://janosh.dev/blog/google-maps+react-hooks
-  function addMarkers(map, points) {
-    points.forEach((point, index) => {
-      const marker = new window.google.maps.Marker({
-        map,
-        position: point.coords,
-        label: point.title,
-        title: point.title,
-      })
-      marker.addListener(`click`, () => {
-        window.location.href = point.url
-      })
-    });
-
+  function addMarkers(map, point) {
+    const marker = new window.google.maps.Marker({
+      map,
+      position: point.coords,
+      label: point.title,
+      title: point.title,
+    })
+    marker.addListener(`click`, () => {
+      window.location.href = point.url
+    })
   }
 
   const point = {
@@ -45,19 +27,11 @@ const RecyclePointPage = () => {
     url: '',
   };
 
-  useEffect(() => {
-    // atualizar as props do mapa
-    const locationMarker = {
-      coords: { lat: location.latitude, lng: location.longitude },
-      title: 'Minha Localização',
-      url: '',
-    }
-    setMapProps({
-      options: { center: { lat: 38.7166700, lng: -9.1333300 }, zoom: 12 },
-      onMount: addMarkers,
-      onMountProps: [locationMarker, point]
-    });
-  }, [location])
+  const mapProps = {
+    options: { center: { lat: point.coords.lat, lng: point.coords.lng }, zoom: 16 },
+    onMount: addMarkers,
+    onMountProps: point,
+  }
 
 
 
